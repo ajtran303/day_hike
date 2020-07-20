8b408d292757b0d2fc6d65840bd467b405ff66aa
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-19 23:41:32 -0600

    Add spec for trip show page trails statistics

--- spec/features/trips/show_page_spec.rb
+++ spec/features/trips/show_page_spec.rb
@@ -4,9 +4,13 @@ RSpec.describe "Trip Show Page Spec" do
   before :each do
     @trip = create(:trip)
 
-    [100, 200, 300].each do |length|
-      @trip.trails << create(:trail, length: length)
+    @lengths = [150, 175, 200, 300]
+
+    @trails = @lengths.map do |length|
+      create(:trail, length: length)
     end
+
+    @trip.trails << @trails
   end
 
   describe "As a visitor" do
@@ -21,6 +25,32 @@ RSpec.describe "Trip Show Page Spec" do
             expect(page).to have_content("Name: #{trail.name}")
             expect(page).to have_content("Address: #{trail.address}")
             expect(page).to have_content("Length: #{trail.length}")
+          end
+        end
+      end
+
+      it "4-7: there are statistics about the trails on that hiking trip" do
+        total_distance = @lengths.sum # 825
+        average_distance = @lengths.sum / @lengths.size.to_f # 206.25
+        longest_trail = @trails.last # 300
+        shortest_trail = @trails.first # 150
+
+        visit "/trips/#{@trip.id}"
+
+        within ".statistics" do
+          expect(page).to have_content("Total hiking distance: #{total_distance}")
+          expect(page).to have_content("Average hiking distance: #{average_distance}")
+
+          within ".longest-trail" do
+            expect(page).to have_content("Longest Trail on Trip:")
+            expect(page).to have_content("Name: #{longest_trail.name}")
+            expect(page).to have_content("Length: #{longest_trail.length}")
+          end
+
+          within ".shortest-trail" do
+            expect(page).to have_content("Shortest Trail on Trip:")
+            expect(page).to have_content("Name: #{shortest_trail.name}")
+            expect(page).to have_content("Length: #{shortest_trail.length}")
           end
         end
       end
