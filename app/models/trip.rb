8128f9d1ccd99a788c4781722c8576416544aca5
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-19 21:32:53 -0600

    Pass validation and assocation tests for models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,8 @@
 class Trip < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :start_date
+  validates_presence_of :end_date
 
-
-
+  has_many :trip_trails
+  has_many :trails, through: :trip_trails
 end
