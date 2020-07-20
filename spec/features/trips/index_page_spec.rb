c1f719dd54c6bec6e21bc84d2f019a3bb8723d53
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-19 23:16:50 -0600

    Fix test setup for Trip Index spec

--- spec/features/trips/index_page_spec.rb
+++ spec/features/trips/index_page_spec.rb
@@ -18,9 +18,9 @@ RSpec.describe "Hiking Index Page Spec" do
       end
 
       it "2: each trip name is a link that goes to that trip's page" do
-        visit "/trips"
-
         @trips.each do |trip|
+          visit "/trips"
+
           expect(page).to have_link(trip.name, href: "/trips/#{trip.id}")
           click_link trip.name
           expect(current_path).to eq("/trips/#{trip.id}")
