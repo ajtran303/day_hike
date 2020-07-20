bc316eb5f809d967fc2a6443cfa1ecc0c1f87208
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-19 21:43:49 -0600

    Remove validation test for Trip start and end dates

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
   validates_presence_of :name
-  validates_presence_of :start_date
-  validates_presence_of :end_date
+  # validates_presence_of :start_date
+  # validates_presence_of :end_date
 
   has_many :trip_trails
   has_many :trails, through: :trip_trails
--- spec/models/trips_spec.rb
+++ spec/models/trips_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.describe Trip do
   describe "validations" do
     it { should validate_presence_of :name }
-    it { should validate_presence_of :start_date }
-    it { should validate_presence_of :end_date }
+    # it { should validate_presence_of :start_date }
+    # it { should validate_presence_of :end_date }
   end
   describe "associations" do
     it { should have_many :trip_trails }
