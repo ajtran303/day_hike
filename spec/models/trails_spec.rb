edd1f13261218384c718dbd12a3fcdb34c96ce0d
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-19 23:57:28 -0600

    Add test for Trails model method .total_distance

--- spec/models/trails_spec.rb
+++ spec/models/trails_spec.rb
@@ -9,9 +9,22 @@ describe Trail, type: :model do
     it { should validate_numericality_of(:length).only_integer }
     it { should validate_numericality_of(:length).is_greater_than(0) }
   end
+
   describe "associations" do
     it { should have_many :trip_trails }
     it { should have_many(:trips).through(:trip_trails) }
   end
 
+  describe "methods" do
+    it ".total_distance" do
+      trip = create(:trip)
+      trip.trails << [150, 175, 200, 300].map do |length|
+        create(:trail, length: length)
+      end
+
+      expect(trip.trails.total_distance).to eq(825)
+
+      DatabaseCleaner.clean
+    end
+  end
 end
